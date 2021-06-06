ba70a43c046ff9f8baf657da550eed52364e462c
unknown <sriv.srivastava@gmail.com>
2021-06-06 23:41:29 +0530

    url vairbales

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -17,5 +17,24 @@ def super_simple():
 @app.route("/not_found")
 def not_found():
     return jsonify(message="not found"), 404
+
+@app.route("/parameters")
+def parameters():
+    name = request.args.get("name")
+    age = int(request.args.get("age"))
+    if age < 18:
+        return jsonify(message="{} you are not old enough".format(name))
+    else:
+        return jsonify(message="Welcome {}".format(name))
+
+
+@app.route("/url_variables/<string:name>/<int:age>")
+def url_variables(name:str, age:int):
+    if age < 18:
+        return jsonify(message="{} you are not old enough".format(name)), 401
+    else:
+        return jsonify(message="Welcome {}".format(name))
+
+
 if __name__ == '__main__':
     app.run()
